4b089ce9abd5ea03aa6e2539f8baf3fdfed2fc50
João Otávio Leites Matuszevski <joaoolmdev@gmail.com>
2024-05-09 16:55:46 -0300

    feat: JWT Authentication

--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 
 
 class MovieListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     
